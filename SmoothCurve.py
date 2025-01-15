8902de3e920de894321d4918fd614bcc320b1267
Ekaterina Moskovkina <e.moskovkina@campus.fct.unl.pt>
2025-01-15 11:01:40 +0000

    smooth curve wip

--- SmoothCurve.py
+++ SmoothCurve.py
@@ -1,10 +1,13 @@
-from typing import Tuple, List
+from typing import Tuple, List, Optional
 from itertools import chain
 
 import numpy as np
+import numpy.typing as npt
 
 import bpy
 import mathutils
+
+from .commons import extract_end_from_name_and_empty, extract_end_name_from_curve_and_id
 
 
 def get_angles(ve: mathutils.Vector) -> Tuple[float, float]:
@@ -75,26 +78,61 @@ def le(co_start: mathutils.Vector, co_end: mathutils.Vector, points: List[VecPoi
     total = 0
     prev = co_start
     for point in points:
-        total += (point.get_prev_co - prev).length
-        prev = point.get_post_co
+        total += (point.get_prev_co() - prev).length
+        prev = point.get_post_co()
     total += len(co_end - prev)
     return total
+
+def set_angles(points: List[VecPoint],
+               x: npt.NDArray):
+    for i, point in enumerate(points):
+        th = x[2 * i]
+        ph = x[2 * i + 1]
+        point.set_th_ph(th, ph)
 
 def get_all_angles(points: List[VecPoint]):
     return np.ndarray(chain(*((p.th, p.ph) for p in points)))
 
 def get_all_diffs(co_start: mathutils.Vector, co_end: mathutils.Vector, points: List[VecPoint]):
     prev = co_start
-    post = points[1].get_prev_co
+    post = points[1].get_prev_co()
     res = []
     for i, point in enumerate(points):
         if i + 1 == len(points):
             post = co_end
         else:
-            post = points[i + 1].get_prev_co
+            post = points[i + 1].get_prev_co()
         res.extend([point.get_diff_th(prev, post), point.get_diff_ph(prev, post)])
-        prev = point.get_post_co
+        prev = point.get_post_co()
     return np.ndarray(res)
+
+def gradient_descent_step(points:List[VecPoint],
+                          step: Optional[float] = None,
+                          co_start: Optional[mathutils.Vector] = None,
+                          co_end: Optional[mathutils.Vector] = None,
+                          xnm1: Optional[npt.NDArray] = None,
+                          dxnm1: Optional[npt.NDArray] = None):
+    x = get_all_angles(points)
+    if co_start is None:
+        co_start = points[-1].get_post_co()
+    if co_end is None:
+        co_start = points[0].get_prev_co()
+    dx = get_all_diffs(co_start, co_end, points)
+    if step is None:
+        step = get_step(x, xnm1, dx, dxnm1)
+    xnp1 = x - step*dx
+    set_angles(points, xnp1)
+    return x, dx
+
+def gradient_descent_steps(points: List[VecPoint],
+                           num_steps: int,
+                           init_step: float,
+                           co_start: Optional[mathutils.Vector] = None,
+                           co_end: Optional[mathutils.Vector] = None):
+
+    xnm1, dxnm1 = gradient_descent_step(points, step=init_step, co_start=co_start, co_end=co_end)
+    for _ in range(num_steps):
+        xnm1, dxnm1 = gradient_descent_step(points, co_start=co_start, co_end=co_end, xnm1=xnm1, dxnm1=dxnm1)
 
 
 def get_step(x, xnm1, dx, dxnm1):
@@ -176,6 +214,17 @@ def get_selected_neighbour_curves(curve: bpy.types.Object,
     return [curve_and_free_i for curve_and_free_i in curves_neighbors
             if curve_and_free_i.curve in selected]
 
+def get_ends(curve1: bpy.types.Object, curve2: bpy.types.Object):
+    gs1 = curve1.greg_curve_settings
+    gs2 = curve2.greg_curve_settings
+    emp_ends1, emp_ends2 = (((gs.end1_empty, gs.end1_name), (gs.end2_empty, gs.end2_name)) for gs in (gs1, gs2))
+    for empty1, end_name1 in emp_ends1:
+        for empty2, end_name2 in emp_ends2:
+            if empty1 == empty2:
+                end1, end2 = (extract_end_from_name_and_empty(empty1, end_name) for end_name in (end_name1, end_name2))
+                return end1, end2
+
+
 class smooth_curves(bpy.types.Operator):
     """Gregory: smooth curves by recalculating handles"""
     bl_idname = "object.greg_smooth_curves"
@@ -199,6 +248,9 @@ class smooth_curves(bpy.types.Operator):
 
     def execute(self, context: bpy.types.Context):        # execute() is called when running the operator.
         selected = context.selected_objects
-        res = return_line_or_circle_from_selected_if_possible(selected)
+        line, is_cyclic = return_line_or_circle_from_selected_if_possible(selected)
+        if is_cyclic:
+            for curve in line:
+
         
         return {'FINISHED'}    
